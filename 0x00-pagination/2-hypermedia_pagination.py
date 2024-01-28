a1d0b1f23eb62428fa13b61db148a0700fab5bc7
manucho254 <manuchoadero@gmail.com>
2024-01-28 20:19:15 +0000

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -83,7 +83,7 @@ class Server:
         return self.__dataset[start:end]
 
     def get_hyper(self, page: int = 1,
-                  page_size: int = 10) -> Mapping[Any]:
+                  page_size: int = 10) -> Mapping[str, Any]:
         """ get data from page
             Args:
                 page: page number
