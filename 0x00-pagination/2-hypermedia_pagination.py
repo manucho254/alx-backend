39bfa6c0f3c0739fc15b4072049a7880e35e7ca7
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-29 20:55:52 +0300

    Deletion-resilient hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -95,7 +95,7 @@ class Server:
             "page_size": page_size,
             "page": page,
             "data": data,
-            "next_page": page + 1 if total_pages > 1 else None,
+            "next_page": page + 1 if total_pages > page else None,
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages,
         }
