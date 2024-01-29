02532ff6857b2c66129b8ad6bdae8250e6e3b17b
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-29 20:11:54 +0300

    Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -9,8 +9,8 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,8 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,16 +28,44 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """ 
+        """Deletion-resilient hypermedia pagination
+        Args:
+            index: index to start from
+            page_size: size of page
+        Return:
+            a dictionary of key/value pairs
         """
+        self.indexed_dataset()
+        indexed_len = len(self.__indexed_dataset)
+        assert index >= 0
+        assert index <= indexed_len
+
+        start, end = index, index + page_size
+        current_index = index
+        data = []
+
+        while start < end:
+            if self.__indexed_dataset.get(start) is None:
+                start += 1
+                end += 1
+                continue
+            arr = self.__indexed_dataset.get(start)
+            current_index = start
+            data.append(arr)
+            start += 1
+
+        next_index = current_index + 1
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data,
+        }
