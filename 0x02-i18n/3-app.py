19eb02711f9bdf901c38d5a04c85a4be81bfb55c
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-06 23:23:10 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,7 +22,7 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """babel locale selector"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"].keys())
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/", strict_slashes=False)
