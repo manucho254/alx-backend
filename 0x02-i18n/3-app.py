06dfdb1a86199f379bfd38f3a4f4405ce21a1b67
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-06 21:38:20 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,15 +25,12 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-# babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route("/", strict_slashes=False)
 def home():
     """home page route"""
     data = {"home_title": _("Welcome to Holberton"),
             "home_header": _("Hello world!")}
-
+    
     return render_template("3-index.html", **data)
 
 
