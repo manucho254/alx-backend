526d078e448f64f538d8f882a42a9221f3259978
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-05 18:33:56 +0300

    promisify

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,37 @@
+import redis from "redis";
+const util = require("util");
+
+const redisURL = "redis://127.0.0.1:6379";
+let client;
+
+(async function () {
+  client = redis.createClient({ url: redisURL });
+  client.on("error", (err) =>
+    console.log("Redis client not connected to the server:", err)
+  );
+
+  client.on("connect", () =>
+    console.log("Redis client connected to the server")
+  );
+})();
+
+// add new key and value pair
+async function setNewSchool(schoolName, value) {
+  await client.set(schoolName, value, redis.print);
+}
+
+// display by key
+async function displaySchoolValue(schoolName) {
+  await client.get(schoolName, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(data);
+  });
+}
+
+displaySchoolValue = util.promisify(displaySchoolValue);
+
+displaySchoolValue("Holberton");
+setNewSchool("HolbertonSanFrancisco", "100");
+displaySchoolValue("HolbertonSanFrancisco");
