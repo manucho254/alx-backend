51abbf571ea04b27f8e71095273601b0bfe01e3a
manucho254 <manuchoadero@gmail.com>
2024-03-05 18:24:51 +0000

    Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -4,28 +4,29 @@ const redisURL = "redis://127.0.0.1:6379";
 let client;
 
 (async function () {
-  client = redis.createClient({ url: redisURL });
-  client.on("error", (err) =>
+  client = await redis.createClient({ url: redisURL }).on("error", (err) =>
     console.log("Redis client not connected to the server:", err)
-  );
-
-  client.on("connect", () =>
+  ).on("connect", () => {
     console.log("Redis client connected to the server")
-  );
-})();
+    const obj = {
+       Portland: 50,
+       Seattle: 80,
+       "New York": 20,
+       Bogota: 20,
+       Cali: 40,
+       Paris: 2,
+     }
 
-client.hSet(
-  "HolbertonSchools",
-  {
-    Portland: 50,
-    Seattle: 80,
-    "New York": 20,
-    Bogota: 20,
-    Cali: 40,
-    Paris: 2,
-  },
-  redis.print({ reply: 1 })
-);
+     for (let [key, value] of Object.entries(obj)) {
+         client.hset("HolbertonSchools", key, value, redis.print("Reply: 1"))
+     }
 
-const userSession = await client.hGetAll("HolbertonSchools");
-console.log(JSON.stringify(userSession, null, 2));
+     client.hgetall("HolbertonSchools", function(err, data) {
+        if (err) {
+	   console.error(err);
+         } else {
+	  console.log(data); // Print all values
+        }
+     })
+  });
+})()
