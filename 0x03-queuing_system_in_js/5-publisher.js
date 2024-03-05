4feb6dbd0ac9ed2d7a3205caaf67eaf779d3efcd
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-06 00:52:45 +0300

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -11,21 +11,21 @@ let client;
     )
     .on("connect", () => {
       console.log("Redis client connected to the server");
+
+      // Define a sleep function that returns a promise
+      function sleep(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+      }
+
+      async function publishMessage(message, time) {
+        console.log(`About to send ${message}`);
+        await sleep(time);
+        client.publish("holberton school channel", message);
+      }
+
+      publishMessage("Holberton Student #1 starts course", 100);
+      publishMessage("Holberton Student #2 starts course", 200);
+      publishMessage("KILL_SERVER", 300);
+      publishMessage("Holberton Student #3 starts course", 400);
     });
 })();
-
-// Define a sleep function that returns a promise
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function publishMessage(message, time) {
-  console.log(`About to send ${message}`);
-  await sleep(time);
-  client.publish("holberton school channel", message);
-}
-
-publishMessage("Holberton Student #1 starts course", 100);
-publishMessage("Holberton Student #2 starts course", 200);
-publishMessage("KILL_SERVER", 300);
-publishMessage("Holberton Student #3 starts course", 400);
