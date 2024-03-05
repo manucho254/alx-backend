ae19a9baaf268b37827b7ea1e6fe3fab9bdeea6f
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-05 21:25:44 +0300

    Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -4,29 +4,33 @@ const redisURL = "redis://127.0.0.1:6379";
 let client;
 
 (async function () {
-  client = await redis.createClient({ url: redisURL }).on("error", (err) =>
-    console.log("Redis client not connected to the server:", err)
-  ).on("connect", () => {
-    console.log("Redis client connected to the server")
-    const obj = {
-       Portland: 50,
-       Seattle: 80,
-       "New York": 20,
-       Bogota: 20,
-       Cali: 40,
-       Paris: 2,
-     }
+  client = await redis
+    .createClient({ url: redisURL })
+    .on("error", (err) =>
+      console.log("Redis client not connected to the server:", err)
+    )
+    .on("connect", () => {
+      console.log("Redis client connected to the server");
 
-     for (let [key, value] of Object.entries(obj)) {
-         client.hset("HolbertonSchools", key, value, redis.print("Reply: 1"))
-     }
+      const obj = {
+        Portland: 50,
+        Seattle: 80,
+        "New York": 20,
+        Bogota: 20,
+        Cali: 40,
+        Paris: 2,
+      };
 
-     client.hgetall("HolbertonSchools", function(err, data) {
+      for (let [key, value] of Object.entries(obj)) {
+        client.hset("HolbertonSchools", key, value, redis.print("Reply: 1"));
+      }
+
+      client.hgetall("HolbertonSchools", function (err, data) {
         if (err) {
-	   console.error(err);
-         } else {
-	  console.log(data); // Print all values
+          console.error(err);
+        } else {
+          console.log(data); // Print all values
         }
-     })
-  });
-})()
+      });
+    });
+})();
