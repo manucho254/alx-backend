a8aadd4f7076df84f15d3dde20006224b4945039
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-05 17:41:18 +0300

    Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,14 @@
 import redis from "redis";
 
-
 (async function () {
-	const redisURL = "redis://127.0.0.1:6379"
-	const client = await redis.createClient({url: redisURL})
-		.on('error', err => console.log("Redis client not connected to the server:", err))
+  const redisURL = "redis://127.0.0.1:6379";
+  const client = await redis.createClient({ url: redisURL });
 
-	client.on("connect", () => console.log("Redis client connected to the server"));
-})()
+  client.on("error", (err) =>
+    console.log("Redis client not connected to the server:", err)
+  );
+
+  client.on("connect", () =>
+    console.log("Redis client connected to the server")
+  );
+})();
