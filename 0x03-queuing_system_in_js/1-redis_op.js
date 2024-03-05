094ea123c8330b28f2292887ea87f65daafa1389
manucho254 <manuchoadero@gmail.com>
2024-03-05 15:28:22 +0000

    Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,23 +1,33 @@
 import redis from "redis";
 
 const redisURL = "redis://127.0.0.1:6379";
-const client = await redis.createClient({ url: redisURL });
+let client;
 
 (async function () {
+  client = redis.createClient({ url: redisURL });
   client.on("error", (err) =>
     console.log("Redis client not connected to the server:", err)
   );
 
   client.on("connect", () =>
-    console.log("Redis client connected to the server")
+      console.log("Redis client connected to the server")
   );
 })();
 
-const setNewSchool = (schoolName, value) => {
-  client.set(schoolName, value).print({ reply: "OK" });
+
+// add new key and value pair
+async function setNewSchool(schoolName, value){
+    await client.set(schoolName, value, redis.print);
 };
-const displaySchoolValue = (schoolName) => {
-  console.log(client.get(schoolName));
+
+// display by key
+async function displaySchoolValue(schoolName) {
+   await client.get(schoolName, (err, data) => {
+      if(err){
+	  throw err;
+      }
+      console.log(data);
+   });
 };
 
 displaySchoolValue("Holberton");
