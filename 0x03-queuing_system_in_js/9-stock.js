48bc068320312641929ce33f5c6596d8252e197a
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-06 21:44:32 +0300

    In stock

---
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -0,0 +1,96 @@
+import redis from "redis";
+
+const redisURL = "redis://127.0.0.1:6379";
+const express = require("express");
+const util = require("util");
+const app = express();
+const port = 1245;
+let client;
+
+const listProducts = [
+  {
+    itemId: 1,
+    itemName: "Suitcase 250",
+    price: 50,
+    initialAvailableQuantity: 4,
+  },
+  {
+    itemId: 2,
+    itemName: "Suitcase 450",
+    price: 100,
+    initialAvailableQuantity: 10,
+  },
+  {
+    itemId: 3,
+    itemName: "Suitcase 650",
+    price: 350,
+    initialAvailableQuantity: 2,
+  },
+  {
+    itemId: 4,
+    itemName: "Suitcase 1050",
+    price: 550,
+    initialAvailableQuantity: 5,
+  },
+];
+
+function getItemById(id) {
+  return listProducts[id];
+}
+
+(async function () {
+  client = redis.createClient({ url: redisURL });
+  client.on("error", (err) =>
+    console.log("Redis client not connected to the server:", err)
+  );
+
+  client.on("connect", () =>
+    console.log("Redis client connected to the server")
+  );
+})();
+
+// save to redis
+function reserveStockById(itemId, stock) {
+  client.set(itemId, stock);
+}
+
+async function getCurrentReservedStockById(itemId) {
+  return await client.get(itemId);
+}
+
+const getCurrent = util.promisify(getCurrentReservedStockById);
+
+app.get("/list_products", (req, res) => {
+  res.send(JSON.stringify(listProducts));
+});
+
+app.get("/list_products/:itemId", (req, res) => {
+  const { itemId } = req.params;
+  const item = getItemById(Number(itemId));
+  if (!item) res.send(JSON.stringify({ status: "Product not found" }));
+  getCurrent(itemId).then((stock) => {
+    if (!stock) item["currentQuantity"] = currentQuantity;
+    else item["currentQuantity"] = item["initialAvailableQuantity"];
+    res.send(JSON.stringify(item));
+  });
+});
+
+app.get("/reserve_product/:itemId", (req, res) => {
+  const { itemId } = req.params;
+  const item = getItemById(Number(itemId));
+  if (!item) res.send(JSON.stringify({ status: "Product not found" }));
+  if (item["initialAvailableQuantity"] <= 1) {
+    res.send(
+      JSON.stringify({
+        status: "Not enough stock available",
+        itemId: itemId,
+      })
+    );
+  }
+  reserveStockById(itemId, 1);
+  res.send(JSON.stringify({ status: "Reservation confirmed", itemId: itemId }));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
