ca3892529aae07d571ac3764c23a2a00bf8001db
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-06 16:18:05 +0300

    Writing the test for job creation

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,45 @@
+import createPushNotificationsJobs from "./8-job.js";
+queue = require("kue").createQueue();
+const { expect } = require("chai");
+
+describe("createPushNotificationsJobs", function () {
+  before(function () {
+    queue.testMode.enter();
+  });
+
+  afterEach(function () {
+    queue.testMode.clear();
+  });
+
+  after(function () {
+    queue.testMode.exit();
+  });
+
+  it("Error if jobs is not array", function () {
+    expect(createPushNotificationsJobs("test", queue)).throw(
+      "Jobs is not an array"
+    );
+  });
+
+  it("Test jobs creation", function () {
+    const jobs = [
+      {
+        phoneNumber: "4153518743",
+        message: "This is the code 4321 to verify your account",
+      },
+      {
+        phoneNumber: "4153538781",
+        message: "This is the code 4562 to verify your account",
+      },
+      {
+        phoneNumber: "4153118782",
+        message: "This is the code 4321 to verify your account",
+      },
+    ];
+    createPushNotificationsJobs(jobs, queue);
+    expect(queue.testMode.jobs.length).to.equal(3);
+    expect(queue.testMode.jobs[0].phoneNumber).to.equal("4153518743");
+    expect(queue.testMode.jobs[1].phoneNumber).to.equal("4153538781");
+    expect(queue.testMode.jobs[2].phoneNumber).to.equal("4153118782");
+  });
+});
